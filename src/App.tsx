f4dc0c943b9f6bff19d11b6a492fc273faf7ac09
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2022-08-13 13:21:08 +0430

    add routes folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
 import "./App.css";
-import MainLayout from "./layouts/MainLayout";
-import { Routes, Route, useLocation } from "react-router-dom";
-import Home from "./pages/Home";
-import Product from "./pages/Product";
-import Category from "./pages/Category";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import Redirect from "./pages/Redirect";
-import Login from "./pages/Login";
-import AdminLayout from "./layouts/AdminLayout";
-import ProductsManagement from "./pages/ProductsManagement";
-import StockPrice from "./pages/StockPrice";
-import Orders from "./pages/Orders";
+import AllRoutes from "./routes";
 
 function App() {
-  const location = useLocation();
-  return (
-    <Routes key={location.pathname} location={location}>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="category:id" element={<Category />} />
-        <Route path="category:id/product:id" element={<Product />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="checkout" element={<Checkout />} />
-        <Route path="redirect" element={<Redirect />} />
-      </Route>
-      <Route path="login" element={<Login />} />
-      <Route element={<AdminLayout />}>
-        <Route path="admin/products" element={<ProductsManagement />} />
-        <Route path="admin/stockprice" element={<StockPrice />} />
-        <Route path="admin/orders" element={<Orders />} />
-      </Route>
-    </Routes>
-  );
+  return <AllRoutes />;
 }
 
 export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import Product from "../pages/Product";
+import Category from "../pages/Category";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import Redirect from "../pages/Redirect";
+import Login from "../pages/Login";
+import AdminLayout from "../layouts/AdminLayout";
+import ProductsManagement from "../pages/ProductsManagement";
+import StockPrice from "../pages/StockPrice";
+import Orders from "../pages/Orders";
+
+const AllRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="category:id" element={<Category />} />
+          <Route path="category:id/product:id" element={<Product />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="redirect" element={<Redirect />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route element={<AdminLayout />}>
+          <Route path="admin/products" element={<ProductsManagement />} />
+          <Route path="admin/stockprice" element={<StockPrice />} />
+          <Route path="admin/orders" element={<Orders />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AllRoutes;
