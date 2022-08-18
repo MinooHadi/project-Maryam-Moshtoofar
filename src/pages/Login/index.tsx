809f184e7bca4bfa6d5d7fa0b425fea950e69893
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2022-08-18 13:03:02 +0430

    UI:create basic login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,46 @@
+import { Button, Form, Input } from "antd";
+
 const Login: React.FC = () => {
-  return <div>Login</div>;
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="نام کاربری"
+        name="username"
+        rules={[{ required: true, message: "نام کاربری الزامی است" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="رمز عبور"
+        name="password"
+        rules={[{ required: true, message: "رمز عبور الزامی است" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          ورود
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default Login;
