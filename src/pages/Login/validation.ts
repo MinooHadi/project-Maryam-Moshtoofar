889a2192a110e9f3124f08a6136df5924fbc3622
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2022-08-30 16:06:02 +0430

    feat:add validation to admin login form

---
+++ src/pages/Login/validation.ts
@@ -0,0 +1,10 @@
+export const validationRules = {
+  userName: [
+    { required: true, message: "نام کاربری الزامی است" },
+    { min: 4, message: "نام کاربری  باید حداقل ۴ حرفی باشد" },
+  ],
+  password: [
+    { required: true, message: "رمز عبور الزامی است" },
+    { min: 4, message: "رمز عبور باید حداقل ۴ حرفی باشد" },
+  ],
+};
